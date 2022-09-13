0fc8e55a2b614de22a35a8edca8bf2aaaba11e8b
Jafet19 <kacur99@gmail.com>
2022-09-13 09:02:29 -0500

    commit tree 3b38edb7c3e62538307c31231060f1624bbd2d2e

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  fetch("http://localhost:3000/dogs", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+    .catch(function (error) {
+      alert("Bad things! Ragnarők!");
+      console.log(error.message);
+    });
+
+function submitData(){
+    const config = {
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(Input)
+    }
+}
+fetch('http://localhost:3000/users', submitData)
+.then(res => res.json());
